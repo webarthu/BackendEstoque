73ff3d1cc753fb598a3f0281e167069283448dc8
webarthu <snoopycomy@gmail.com>
2025-04-09 20:54:29 -0300

    stagnant today

--- routes/vendas.py
+++ routes/vendas.py
@@ -12,18 +12,26 @@ def registerVendas():
     try:
         cursor = con.cursor()
         nome = request.json.get('nome')
-        id = request.json.get('id')
-        
-        if not nome or not id:
+        id_cliente = request.json.get('id')
+        produtos = request.json.get([{'id_produto', 'quantidade'}])
+
+        if not nome and not id_cliente:
             return jsonify({"Error": "É necessário informar um nome ou id para cadastrar uma venda"}),400
         
         if nome:
-            cursor.execute("SELECT id FROM Clientes WHERE nome = %s", (nome))
+            cursor.execute("SELECT * FROM Clientes WHERE nome = %s", (nome))
 
-        if id:
-            cursor.execute("SELECT name FROM Clientes WHERE id = %s", (id))
+        if id_cliente:
+            cursor.execute("INSERT INTO Vendas (id_cliente, data_venda) VALUES (s%, )", (id_cliente, ))
+        
+        cursor.execute("SELECT id FROM Vendas WHERE ...")
+        id_venda = cursor.fetchone()
+        
+        cursor.execute("SELECT valor FROM Produtos WHERE id = %s ", (for i in produtos[0]))
 
-        cursor.execute()
+        cursor.execute("INSERT INTO ItensVenda (id_produto, quantidade, valor_unitario) VALUES (s%, s%, s%)",(produtos.id_produto, produtos.quantidade, valor_unitario))
+
+        return jsonify({"Venda realizada!": "ID venda":id_venda}),200
 
     except Exception as e:
         return jsonify({"Error", str(e)}),500
