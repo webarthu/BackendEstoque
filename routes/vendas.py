6648e8a207c95738eb84ecd80352834c106ff778
webarthu <snoopycomy@gmail.com>
2025-04-13 03:53:02 -0300

    fixed: PUT vendas endpoint

--- routes/vendas.py
+++ routes/vendas.py
@@ -13,34 +13,61 @@ def registerVendas():
         cursor = con.cursor()
         nome = request.json.get('nome')
         id_cliente = request.json.get('id')
-        produtos = request.json.get({'id_produto', 'quantidade'})
+        produtos = request.json.get('produtos')
+
+        print("nome:", nome)
+        print("\nid:", id_cliente)
+        print("\nprodutos:", produtos)
 
         if not nome and not id_cliente:
             return jsonify({"Error": "É necessário informar um nome ou id para cadastrar uma venda"}),400
         
         if nome:
-            cursor.execute("SELECT * FROM Clientes WHERE nome = %s", (nome))
+            cursor.execute("SELECT * FROM Clientes WHERE nome = %s", (nome,))
+            clientes = cursor.fetchall()
+            
+            print("\nclientes:", clientes)
 
+            if len(clientes) > 1: #{
+                    return jsonify({"Options": clientes, "Message": "Mais de um cliente encontrado com esse nome"}), 400
+            #}
+            id_cliente = clientes[0][0]
+            
+            print("\nid_cliente:", id_cliente)
+            
         if id_cliente:
-            cursor.execute("INSERT INTO Vendas (id_cliente, data_venda) VALUES (s%, )", (id_cliente, ))
+            cursor.execute("INSERT INTO Vendas (id_cliente) VALUES (%s)", (id_cliente,))
             id_venda = cursor.lastrowid
-        
-        valor_produto = (produtos[0] for i in produtos)
-
-        cursor.execute("SELECT valor FROM Produtos WHERE id = %s", (produtos[0] for i in produtos))
-
+            
+            print("\nid_venda:", id_venda)
 
         for produto in produtos:
-            cursor.execute("SELECT valor FROM Produtos WHERE id=%s",["id_produto"])
+            print("entrou no loop")
+            print("aparente erro:", produto["id_produto"])
+            print(produto)
+            print(produto["id_produto"])
+            print(produto["quantidade"])
+            print("---------")
+            cursor.execute("SELECT valor FROM Produtos WHERE id=%s", (produto['id_produto'],))
+            print("executou a query")
 
             valor_unitario = cursor.fetchone()[0]
+            
+            print("\nvalor_unitario", valor_unitario)
+            
 
-            cursor.execute("INSERT INTO ItensVenda (id_produto, quantidade, valor_unitario) VALUES (%s, %s, %s)",(produtos["id_produto"], produtos["quantidade"], valor_unitario))
+            cursor.execute("INSERT INTO ItensVenda (id_venda, id_produto, quantidade, valor_unitario) VALUES (%s, %s, %s, %s)",(id_venda, produto["id_produto"], produto["quantidade"], valor_unitario))
+
+            print("segunda query do loop")
+
+
+        con.commit()
 
         return jsonify({"Message": "Venda realizada!", "ID venda": id_venda}),200
 
     except Exception as e:
-        return jsonify({"Error", str(e)}),500
+        return jsonify({"Error": str(e)}), 500
+ 
     finally:
         cursor.close()
         con.close()
