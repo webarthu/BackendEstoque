8ff9eb64c5b4f0b66c8ae3a45b285e4f6677517d
webarthu <snoopycomy@gmail.com>
2025-04-22 16:38:16 -0300

    feat: Vendas DELETE endpoint

--- routes/vendas.py
+++ routes/vendas.py
@@ -12,7 +12,6 @@ def getVendas():
         
         cursor.execute("SELECT * FROM Vendas")
         vendas = cursor.fetchall()
-        print (vendas)
         resultado = []
         for venda in vendas:
             resultado.append({
@@ -31,69 +30,98 @@ def getVendas():
     finally:
         cursor.close()
         con.close()
+
+
 #POST
 @vendas_bp.route('/registerVendas', methods=['POST'])
 def registerVendas():
     con = get_connection()
     try:
         cursor = con.cursor()
-        nome = request.json.get('nome')
+        nome_cliente = request.json.get('nome')
         id_cliente = request.json.get('id')
         produtos = request.json.get('produtos')
+        valor_total = 0
 
-        print("nome:", nome)
-        print("\nid:", id_cliente)
-        print("\nprodutos:", produtos)
-
-        if not nome and not id_cliente:
+        if not nome_cliente and not id_cliente:
             return jsonify({"Error": "É necessário informar um nome ou id para cadastrar uma venda"}),400
         
-        if nome:
-            cursor.execute("SELECT * FROM Clientes WHERE nome = %s", (nome,))
+        if not produtos:
+            return jsonify({"Error": "nao tem produtos"}),400
+
+        if nome_cliente:
+            cursor.execute("SELECT * FROM Clientes WHERE nome = %s", (nome_cliente,))
             clientes = cursor.fetchall()
-            
-            print("\nclientes:", clientes)
+
 
             if len(clientes) > 1: #{
-                    return jsonify({"Options": clientes, "Message": "Mais de um cliente encontrado com esse nome"}), 400
+                    return jsonify({"Options": clientes, "Message": "Mais de um cliente encontrado com esse nome"}),400
             #}
             id_cliente = clientes[0][0]
             
-            print("\nid_cliente:", id_cliente)
             
         if id_cliente:
             cursor.execute("INSERT INTO Vendas (id_cliente) VALUES (%s)", (id_cliente,))
             id_venda = cursor.lastrowid
-            
-            print("\nid_venda:", id_venda)
 
         for produto in produtos:
-            print("entrou no loop")
-            print("aparente erro:", produto["id_produto"])
-            print(produto)
-            print(produto["id_produto"])
-            print(produto["quantidade"])
-            print("---------")
             cursor.execute("SELECT valor FROM Produtos WHERE id=%s", (produto['id_produto'],))
-            print("executou a query")
-
-            valor_unitario = cursor.fetchone()[0]
+            resultado = cursor.fetchone()
             
-            print("\nvalor_unitario", valor_unitario)
-            
+            if resultado: 
+                valor_unitario = resultado[0]
+                quantidade = produto['quantidade']
+                valor_total += valor_unitario * quantidade
+                
+                cursor.execute("INSERT INTO ItensVenda (id_venda, id_produto, quantidade, valor_unitario) VALUES (%s, %s, %s, %s)",(id_venda, produto["id_produto"], quantidade, valor_unitario))
 
-            cursor.execute("INSERT INTO ItensVenda (id_venda, id_produto, quantidade, valor_unitario) VALUES (%s, %s, %s, %s)",(id_venda, produto["id_produto"], produto["quantidade"], valor_unitario))
-
-            print("segunda query do loop")
-
-
+        cursor.execute("UPDATE Vendas SET valor_total = %s WHERE id = %s", (valor_total, id_venda))
+        
         con.commit()
-
-        return jsonify({"Message": "Venda realizada!", "ID venda": id_venda}),200
+        
+        return jsonify({"Message": "Venda realizada!", "ID venda": id_venda, "Valor total": valor_total, "ID_Cliente": id_cliente, "Nome Cliente":nome_cliente}), 200
 
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
  
+    finally:
+        cursor.close()
+        con.close()
+
+
+#DELETE
+@vendas_bp.route('/deleteVendas/<int:id>', methods=['DELETE'])
+def deleteVendas(id):
+    con = get_connection()
+    try:
+        cursor = con.cursor()
+
+        cursor.execute("SELECT id_cliente FROM Vendas WHERE id=%s", (id,))
+        
+        result = cursor.fetchone()
+        if not result:
+            return jsonify({'Error': "Venda nao encontrada"}),400
+
+        id_cliente = result[0]
+
+        cursor.execute("SELECT nome FROM Clientes WHERE id=%s", (id_cliente,))
+        
+        res = cursor.fetchone()
+        if not res:
+            return jsonify({'Error': "Cliente nao encontrado"}),400
+
+        nome = res[0]
+
+        cursor.execute("DELETE FROM ItensVenda WHERE id_venda=%s", (id,))
+        cursor.execute("DELETE FROM Vendas WHERE id=%s", (id,))
+        con.commit()
+
+        return jsonify({"Message": "Venda Deleteada", "Nome Cliente": nome, "ID Cliente": id_cliente}),200
+
+        
+    except Exception as e:
+        return jsonify({"Error": str(e)}),500
+    
     finally:
         cursor.close()
         con.close()
